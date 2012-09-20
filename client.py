2982b975362ac7cecb3ed135c189285b895ad18e
Jordan Haber <jordan.haber@gmail.com>
2012-09-19 21:09:24 -0600

    cleaned up client

--- client.py
+++ client.py
@@ -2,8 +2,7 @@ import wave, threading
 from pyaudio import *
 from socket import *
 from collections import deque
-import binascii
-import StringIO
+
 
 class Client(threading.Thread):
 
@@ -18,10 +17,8 @@ class Client(threading.Thread):
     def run(self):
 
         while True:
-			data = self.client.recv(1024)
-         #   print data
-			#binstring = binascii.unhexlify(data)
-			self.queue.append(data)
+            data = self.client.recv(1024)
+            self.queue.append(data)
 
 
 class Streamer(threading.Thread):
@@ -33,38 +30,36 @@ class Streamer(threading.Thread):
         self.chunk = 1024
 
     def run(self):
-		p = PyAudio()
-    
-		FORMAT = paInt16
-		CHANNELS = 1
-		RATE = 8000
 
-		data = wave.open('outfile.wav', 'wb')
-		data.setnchannels(CHANNELS)
-		data.setsampwidth(p.get_sample_size(FORMAT))
-		data.setframerate(RATE)
+        p = PyAudio()
 
-		data_format = p.get_format_from_width(data.getsampwidth())
-		data_channels = data.getnchannels()
-		data_rate = data.getframerate()
+        stream = p.open(format = data_format, channels = data_channels, rate = data_rate, output = True)
+        
+        while True:
 
-		stream = p.open(format = data_format, channels = data_channels, rate = data_rate, output = True)
-
-		while True:
-
-			if len(self.client.queue) > self.buffer_size:
+            if len(self.client.queue) > self.buffer_size:
                 
-				data.writeframes(self.client.queue[0])
-				stream.write(self.client.queue.popleft())
-			#	audio = data.readframes(self.chunk)
-			#	while not audio == '':
-			#		stream.write(audio)
-			#		audio = data.readframes(self.chunk)
+                stream.write(self.client.queue.popleft())
+		
+        stream.close()
+        p.terminate()
 
+    def save(self):
 
-		stream.close()
-		p.terminate()
+        FORMAT = paInt16
+        CHANNELS = 1
+        RATE = 8000
+
+        data = wave.open('outfile.wav', 'wb')
+        data.setnchannels(CHANNELS)
+        data.setsampwidth(p.get_sample_size(FORMAT))
+        data.setframerate(RATE)
+
+        data_format = p.get_format_from_width(data.getsampwidth())
+        data_channels = data.getnchannels()
+        data_rate = data.getframerate()
                 
+        data.writeframes(self.client.queue[0])
 
 
 if __name__ == '__main__':
